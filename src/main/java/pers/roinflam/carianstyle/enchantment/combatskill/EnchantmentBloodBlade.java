3891c6ce29e1ff2c1bc1e3c2b731aaec1ef3ac7e
Roinflam <roinflam@outlook.com>
2022-10-23 12:32:17 +0800

    Update version 1.1.0

--- src/main/java/pers/roinflam/carianstyle/enchantment/combatskill/EnchantmentBloodBlade.java
+++ src/main/java/pers/roinflam/carianstyle/enchantment/combatskill/EnchantmentBloodBlade.java
@@ -54,7 +54,7 @@ public class EnchantmentBloodBlade extends RaryBase {
 
     @Override
     public boolean canApplyTogether(Enchantment ench) {
-        return !CarianStyleEnchantments.COMBAT_SKILL.contains(ench) &&
+        return super.canApplyTogether(ench) &&
                 !ench.equals(CarianStyleEnchantments.SCARLET_ROT) &&
                 !ench.equals(CarianStyleEnchantments.FIRE_GIVES_POWER) &&
                 !ench.equals(CarianStyleEnchantments.FIRE_DEVOURED) &&
