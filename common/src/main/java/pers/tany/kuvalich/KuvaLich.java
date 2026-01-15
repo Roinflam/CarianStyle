076f348ffd520d0b43ec9e3bb59344bd441ecb1f
Roinflam <q2651078496@outlook.com>
2026-01-15 23:08:48 +0800

    初次

---
+++ common/src/main/java/pers/tany/kuvalich/KuvaLich.java
@@ -0,0 +1,9 @@
+package pers.tany.carianstyle;
+
+public final class CarianStyle {
+    public static final String MOD_ID = "carianstyle";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
